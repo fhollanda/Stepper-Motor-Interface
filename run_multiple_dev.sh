8e7e7ccd0df7a588afa0484139a1bf3236a6a40f
Felipe Hollanda <fhollanda@live.com>
2017-07-07 15:25:07 -0300

    incluindo "smi" na pasta de logs para melhor organização

--- run_multiple_dev.sh
+++ run_multiple_dev.sh
@@ -1,15 +1,15 @@
 . /tcc/motor/env/bin/activate
-nohup python /tcc/motor/test/mock.py > /var/log/motor.log 2>&1 &
+nohup python /tcc/motor/test/mock.py > /var/log/smi/motor.log 2>&1 &
 deactivate
 
 . /tcc/tektronix/comm/bin/activate
-nohup python /tcc/tektronix/test/mock.py > /var/log/tektronix.log 2>&1 &
+nohup python /tcc/tektronix/test/mock.py > /var/log/smi/tektronix.log 2>&1 &
 deactivate
 
 . /tcc/adapter/env/bin/activate
-nohup python /tcc/adapter/bridge.py > /var/log/adapter.log 2>&1 &
+nohup python /tcc/adapter/bridge.py > /var/log/smi/adapter.log 2>&1 &
 deactivate
 
 . /tcc/web_app/env/bin/activate
-nohup python /tcc/web_app/web_app.py > /var/log/web_app.log 2>&1 &
+nohup python /tcc/web_app/web_app.py > /var/log/smi/web_app.log 2>&1 &
 deactivate
