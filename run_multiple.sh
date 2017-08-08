7dcf1d9c262875f45a5ae1e973cad96bf824e877
Felipe Hollanda <fhollanda@live.com>
2017-08-08 17:47:46 -0300

    Update run_multiple.sh

--- run_multiple.sh
+++ run_multiple.sh
@@ -1,15 +1,15 @@
-. /tcc/motor/env/bin/activate
+. /Stepper-Motor-Interface/motor/env/bin/activate
 nohup python /tcc/motor/rest_api.py > /var/log/smi/motor.log 2>&1 &
 deactivate
 
-. /tcc/tektronix/comm/bin/activate
+. /Stepper-Motor-Interface/tektronix/env/bin/activate
 nohup python /tcc/tektronix/rest_api.py > /var/log/smi/tektronix.log 2>&1 &
 deactivate
 
-. /tcc/adapter/env/bin/activate
+. /Stepper-Motor-Interface/adapter/env/bin/activate
 nohup python /tcc/adapter/bridge.py > /var/log/smi/adapter.log 2>&1 &
 deactivate
 
-. /tcc/web_app/env/bin/activate
+. /Stepper-Motor-Interface/web_app/env/bin/activate
 nohup python /tcc/web_app/web_app.py > /var/log/smi/web_app.log 2>&1 &
 deactivate
