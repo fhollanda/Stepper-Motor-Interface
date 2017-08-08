17c1d540fe3353f9ba512af1fc211a1171bc5f67
Felipe Hollanda <fhollanda@live.com>
2017-08-08 17:48:25 -0300

    Update run_multiple_dev.sh

--- run_multiple_dev.sh
+++ run_multiple_dev.sh
@@ -1,15 +1,15 @@
-. /tcc/motor/env/bin/activate
-nohup python /tcc/motor/test/mock.py > /var/log/smi/motor.log 2>&1 &
+. /Stepper-Motor-Interface/motor/env/bin/activate
+nohup python /Stepper-Motor-Interface/motor/test/mock.py > /var/log/smi/motor.log 2>&1 &
 deactivate
 
-. /tcc/tektronix/comm/bin/activate
-nohup python /tcc/tektronix/test/mock.py > /var/log/smi/tektronix.log 2>&1 &
+. /Stepper-Motor-Interface/tektronix/env/bin/activate
+nohup python /Stepper-Motor-Interface/tektronix/test/mock.py > /var/log/smi/tektronix.log 2>&1 &
 deactivate
 
-. /tcc/adapter/env/bin/activate
-nohup python /tcc/adapter/bridge.py > /var/log/smi/adapter.log 2>&1 &
+. /Stepper-Motor-Interface/adapter/env/bin/activate
+nohup python /Stepper-Motor-Interface/adapter/bridge.py > /var/log/smi/adapter.log 2>&1 &
 deactivate
 
-. /tcc/web_app/env/bin/activate
-nohup python /tcc/web_app/web_app.py > /var/log/smi/web_app.log 2>&1 &
+. /Stepper-Motor-Interface/web_app/env/bin/activate
+nohup python /Stepper-Motor-Interface/web_app/web_app.py > /var/log/smi/web_app.log 2>&1 &
 deactivate
