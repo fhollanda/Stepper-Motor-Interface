f098a81b277afc8106f87023a7980a0a43a95d21
Felipe Hollanda <fhollanda@live.com>
2017-08-08 17:54:47 -0300

    Update run_multiple_dev.sh

--- run_multiple_dev.sh
+++ run_multiple_dev.sh
@@ -1,15 +1,15 @@
-. /Stepper-Motor-Interface/motor/env/bin/activate
-nohup python /Stepper-Motor-Interface/motor/test/mock.py > /var/log/smi/motor.log 2>&1 &
+. motor/env/bin/activate
+nohup python motor/test/mock.py > /var/log/smi/motor.log 2>&1 &
 deactivate
 
-. /Stepper-Motor-Interface/tektronix/env/bin/activate
-nohup python /Stepper-Motor-Interface/tektronix/test/mock.py > /var/log/smi/tektronix.log 2>&1 &
+. tektronix/env/bin/activate
+nohup python tektronix/test/mock.py > /var/log/smi/tektronix.log 2>&1 &
 deactivate
 
-. /Stepper-Motor-Interface/adapter/env/bin/activate
-nohup python /Stepper-Motor-Interface/adapter/bridge.py > /var/log/smi/adapter.log 2>&1 &
+. adapter/env/bin/activate
+nohup python adapter/bridge.py > /var/log/smi/adapter.log 2>&1 &
 deactivate
 
-. /Stepper-Motor-Interface/web_app/env/bin/activate
-nohup python /Stepper-Motor-Interface/web_app/web_app.py > /var/log/smi/web_app.log 2>&1 &
+. web_app/env/bin/activate
+nohup python web_app/web_app.py > /var/log/smi/web_app.log 2>&1 &
 deactivate
